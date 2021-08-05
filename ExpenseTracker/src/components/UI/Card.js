50e1b8d622a1430e9947b959751c425e4d2ff483
DeonJ76 <deon.jordaan76@gmail.com>
2021-08-05 21:45:00 +0200

    Changes to file system

--- ExpenseTracker/src/components/UI/Card.js
+++ ExpenseTracker/src/components/UI/Card.js
@@ -1,4 +1,4 @@
-import '../UI/Card.css';
+import './Card.css';
 
 const Card = (props) => {
 	const classes = 'card ' + props.className;
---
+++ Movie API/src/App.js
@@ -0,0 +1,89 @@
+import React, { useState } from 'react';
+
+import MoviesList from './components/MoviesList';
+import './App.css';
+
+function App() {
+	const [movies, setMovies] = useState([]);
+
+	// NOTE Prmises with 'async/await'
+	async function fetchMoviesHandler() {
+		const response = await fetch('https://swapi.dev/api/films/');
+
+		const data = await response.json();
+
+		const transformedMovies = data.results.map((movieData) => {
+			return {
+				id: movieData.episode_id,
+				title: movieData.title,
+				openingText: movieData.opening_crawl,
+				releaseDate: movieData.release_date,
+			};
+		});
+		setMovies(transformedMovies);
+		console.log(data);
+	}
+
+	return (
+		<React.Fragment>
+			<section>
+				<button onClick={fetchMoviesHandler}>Fetch Movies</button>
+			</section>
+			<section>
+				<MoviesList movies={movies} />
+			</section>
+		</React.Fragment>
+	);
+}
+
+export default App;
+
+// NOTE Default movie data
+// const dummyMovies = [
+//   {
+//     id: 1,
+//     title: 'Some Dummy Movie',
+//     openingText: 'This is the opening text of the movie',
+//     releaseDate: '2021-05-18',
+//   },
+//   {
+//     id: 2,
+//     title: 'Some Dummy Movie 2',
+//     openingText: 'This is the second opening text of the movie',
+//     releaseDate: '2021-05-19',
+//   },
+// ];
+
+// NOTE Promises with 'then'
+// function App() {
+// 	const [movies, setMovies] = useState([]);
+
+// 	function fetchMoviesHandler() {
+// 		fetch('https://swapi.dev/api/films/')
+// 			.then((response) => {
+// 				return response.json();
+// 			})
+// 			.then((data) => {
+// 				const transformedMovies = data.results.map((movieData) => {
+// 					return {
+// 						id: movieData.episode_id,
+// 						title: movieData.title,
+// 						openingText: movieData.opening_crawl,
+// 						releaseDate: movieData.release_date,
+// 					};
+// 				});
+// 				setMovies(transformedMovies);
+// 			});
+// 	}
+
+// 	return (
+// 		<React.Fragment>
+// 			<section>
+// 				<button onClick={fetchMoviesHandler}>Fetch Movies</button>
+// 			</section>
+// 			<section>
+// 				<MoviesList movies={movies} />
+// 			</section>
+// 		</React.Fragment>
+// 	);
+// }
