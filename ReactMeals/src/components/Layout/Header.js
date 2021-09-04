85f5005b62a49c6e3ee6bf6fe26fe4fb5e371e42
DeonJ76 <deon.jordaan76@gmail.com>
2021-09-04 22:24:39 +0200

    Section 11 - Building practice project components

---
+++ ReactMeals/src/components/Layout/Header.js
@@ -0,0 +1,21 @@
+import { Fragment } from 'react';
+
+import classes from './Header.module.css';
+import HeaderCartButton from './HeaderCartButton';
+import mealsImage from '../../assets/meals.jpg';
+
+const Header = (props) => {
+	return (
+		<Fragment>
+			<header className={classes.header}>
+				<h1>ReactMeals</h1>
+				<HeaderCartButton />
+			</header>
+			<div className={classes['main-image']}>
+				<img alt="A table full of delicious food" src={mealsImage} />
+			</div>
+		</Fragment>
+	);
+};
+
+export default Header;
