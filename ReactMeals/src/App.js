f3a70ecfefd8e30c9fef49c56bdf1e0023a23cad
DeonJ76 <deon.jordaan76@gmail.com>
2021-09-05 20:42:30 +0200

    Section 11 - practice project - implementing cart & modal state & context

--- ReactMeals/src/App.js
+++ ReactMeals/src/App.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
+import Cart from './components/Cart/Cart';
+import CartProvider from './store/CartProvider';
 
 function App() {
+	const [cartIsShown, setCartIsShown] = useState(false);
+
+	const showCartHandler = () => {
+		setCartIsShown(true);
+	};
+
+	const hideCartHandler = () => {
+		setCartIsShown(false);
+	};
+
 	return (
-		<React.Fragment>
-			<Header />
+		<CartProvider>
+			{cartIsShown && <Cart onCloseCart={hideCartHandler} />}
+			<Header onShowCart={showCartHandler} />
 			<main>
 				<Meals />
 			</main>
-		</React.Fragment>
+		</CartProvider>
 	);
 }
 
--- ReactMeals/src/components/Cart/Cart.js
+++ ReactMeals/src/components/Cart/Cart.js
@@ -1,3 +1,4 @@
+import Modal from '../UI/Modal';
 import classes from './Cart.module.css';
 
 const Cart = (props) => {
@@ -11,17 +12,22 @@ const Cart = (props) => {
 		</ul>
 	);
 	return (
-		<div>
+		<Modal onClose={props.onCloseCart}>
 			{cartItems}
 			<div className={classes.total}>
 				<span>Total Amount</span>
 				<span>35.62</span>
 			</div>
 			<div className={classes.actions}>
-				<button className={classes['button--alt']}>Close</button>
+				<button
+					className={classes['button--alt']}
+					onClick={props.onCloseCart}
+				>
+					Close
+				</button>
 				<button className={classes.button}>Order</button>
 			</div>
-		</div>
+		</Modal>
 	);
 };
 
