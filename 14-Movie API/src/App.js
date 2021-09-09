e21725ba8b4d7532cd82d5953eb3b482768c3b40
DeonJ76 <deon.jordaan76@gmail.com>
2021-09-09 17:59:01 +0200

    Section 14 - Sending requests, async/await & errors

--- 14-Movie API/src/App.js
+++ 14-Movie API/src/App.js
@@ -5,23 +5,49 @@ import './App.css';
 
 function App() {
 	const [movies, setMovies] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+	const [error, setError] = useState(null);
 
-	// NOTE Prmises with 'async/await'
+	// NOTE Promises with 'async/await'
 	async function fetchMoviesHandler() {
-		const response = await fetch('https://swapi.dev/api/films/');
+		setIsLoading(true);
+		setError(null);
+		try {
+			const response = await fetch('https://swapi.dev/api/films/');
 
-		const data = await response.json();
+			if (!response.ok) {
+				throw new Error('Something went wrong!');
+			}
 
-		const transformedMovies = data.results.map((movieData) => {
-			return {
-				id: movieData.episode_id,
-				title: movieData.title,
-				openingText: movieData.opening_crawl,
-				releaseDate: movieData.release_date,
-			};
-		});
-		setMovies(transformedMovies);
-		console.log(data);
+			const data = await response.json();
+
+			const transformedMovies = data.results.map((movieData) => {
+				return {
+					id: movieData.episode_id,
+					title: movieData.title,
+					openingText: movieData.opening_crawl,
+					releaseDate: movieData.release_date,
+				};
+			});
+			setMovies(transformedMovies);
+		} catch (error) {
+			setError(error.message);
+		}
+		setIsLoading(false);
+	}
+
+	let content = <p>'No movies found'</p>;
+
+	if (movies.length > 0) {
+		content = <MoviesList movies={movies} />;
+	}
+
+	if (error) {
+		content = <p>{error}</p>;
+	}
+
+	if (isLoading) {
+		content = <p>Loading...</p>;
 	}
 
 	return (
@@ -29,14 +55,22 @@ function App() {
 			<section>
 				<button onClick={fetchMoviesHandler}>Fetch Movies</button>
 			</section>
-			<section>
-				<MoviesList movies={movies} />
-			</section>
+			<section>{content}</section>
 		</React.Fragment>
 	);
 }
 
 export default App;
+
+// NOTE Initial content replaced with 'content' variable
+// {!isLoading && movies.length > 0 && (
+// 	<MoviesList movies={movies} />
+// )}
+// {!isLoading && movies.length === 0 && !error && (
+// 	<p>No movies found</p>
+// )}
+// {isLoading && <p>Loading...</p>}
+// {!isLoading && error && <p>{error}</p>}
 
 // NOTE Default movie data
 // const dummyMovies = [
