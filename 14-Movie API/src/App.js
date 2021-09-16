9c0f6a792c2550aefa164dc3d3a8b5afa8fa280b
DeonJ76 <deon.jordaan76@gmail.com>
2021-09-16 12:28:20 +0200

    Section 14 complete - send a POST request

--- 14-Movie API/src/App.js
+++ 14-Movie API/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import MoviesList from './components/MoviesList';
 import './App.css';
@@ -9,7 +9,7 @@ function App() {
 	const [error, setError] = useState(null);
 
 	// NOTE Promises with 'async/await'
-	async function fetchMoviesHandler() {
+	const fetchMoviesHandler = useCallback(async () => {
 		setIsLoading(true);
 		setError(null);
 		try {
@@ -34,7 +34,11 @@ function App() {
 			setError(error.message);
 		}
 		setIsLoading(false);
-	}
+	}, []);
+
+	useEffect(() => {
+		fetchMoviesHandler();
+	}, [fetchMoviesHandler]);
 
 	let content = <p>'No movies found'</p>;
 
