a21842046650a673c9561b374dc465dcb8a1c3c7
DeonJ76 <deon.jordaan76@gmail.com>
2021-10-07 11:42:36 +0200

    Section 15 complete - Using Custom Hooks

--- 15-Custom Hooks/04-onwards-to-a-more-realistic-example/src/App.js
+++ 15-Custom Hooks/04-onwards-to-a-more-realistic-example/src/App.js
@@ -2,42 +2,37 @@ import React, { useEffect, useState } from 'react';
 
 import Tasks from './components/Tasks/Tasks';
 import NewTask from './components/NewTask/NewTask';
+import useHttp from './hooks/use-http';
 
 function App() {
-	// const [isLoading, setIsLoading] = useState(false);
-	// const [error, setError] = useState(null);
-	// const [tasks, setTasks] = useState([]);
+	const [tasks, setTasks] = useState([]);
 
-	// const fetchTasks = async (taskText) => {
-	// 	setIsLoading(true);
-	// 	setError(null);
-	// 	try {
-	// 		const response = await fetch(
-	// 			'https://react-http-37831-default-rtdb.firebaseio.com/tasks.json'
-	// 		);
+	const { isLoading, error, sendRequest: fetchTasks } = useHttp();
 
-	// 		if (!response.ok) {
-	// 			throw new Error('Request failed!');
-	// 		}
-
-	// 		const data = await response.json();
-
-	// 		const loadedTasks = [];
-
-	// 		for (const taskKey in data) {
-	// 			loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-	// 		}
-
-	// 		setTasks(loadedTasks);
-	// 	} catch (err) {
-	// 		setError(err.message || 'Something went wrong!');
-	// 	}
-	// 	setIsLoading(false);
-	// };
+	// Save this line by moving this destructuring to the line above
+	// const { isLoading, data, sendRequest } = httpData;
 
 	useEffect(() => {
-		fetchTasks();
-	}, []);
+		const transformTasks = (tasksObject) => {
+			const loadedTasks = [];
+
+			for (const taskKey in tasksObject) {
+				loadedTasks.push({
+					id: taskKey,
+					text: tasksObject[taskKey].text,
+				});
+			}
+
+			setTasks(loadedTasks);
+		};
+
+		fetchTasks(
+			{
+				url: 'https://react-http-37831-default-rtdb.firebaseio.com/tasks.json',
+			},
+			transformTasks
+		);
+	}, [fetchTasks]);
 
 	const taskAddHandler = (task) => {
 		setTasks((prevTasks) => prevTasks.concat(task));
@@ -57,3 +52,31 @@ function App() {
 }
 
 export default App;
+
+//Old fetchTasks function, FYI
+// const fetchTasks = async (taskText) => {
+// 	setIsLoading(true);
+// 	setError(null);
+// 	try {
+// 		const response = await fetch(
+// 			'https://react-http-37831-default-rtdb.firebaseio.com/tasks.json'
+// 		);
+
+// 		if (!response.ok) {
+// 			throw new Error('Request failed!');
+// 		}
+
+// 		const data = await response.json();
+
+// 		const loadedTasks = [];
+
+// 		for (const taskKey in data) {
+// 			loadedTasks.push({ id: taskKey, text: data[taskKey].text });
+// 		}
+
+// 		setTasks(loadedTasks);
+// 	} catch (err) {
+// 		setError(err.message || 'Something went wrong!');
+// 	}
+// 	setIsLoading(false);
+// };
