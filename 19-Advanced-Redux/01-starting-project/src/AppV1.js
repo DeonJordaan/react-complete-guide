6b68212fad66da3fc5f0d6509846052e18495f79
DeonJ76 <deon.jordaan76@gmail.com>
2021-10-21 18:28:26 +0200

    Section 19 Complete - Advanced Redux - HTTP, Thunks & Fetching Data

--- 19-Advanced-Redux/01-starting-project/src/AppV1.js
+++ 19-Advanced-Redux/01-starting-project/src/AppV1.js
@@ -1,33 +1,65 @@
 import { Fragment, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
+import { uiActions } from './store/ui-slice';
+
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import Notification from './components/UI/Notification';
-import { sendCartData, fetchCartData } from './store/cart-actions';
 
 let isInitial = true;
 
 function App() {
 	const dispatch = useDispatch();
+
 	const showCart = useSelector((state) => state.ui.cartIsVisible);
 	const cart = useSelector((state) => state.cart);
 	const notification = useSelector((state) => state.ui.notification);
 
 	useEffect(() => {
-		dispatch(fetchCartData());
-	}, [dispatch]);
+		const sendCartData = async () => {
+			dispatch(
+				uiActions.showNotification({
+					status: 'pending',
+					title: 'Sending...',
+					message: 'Sending cart data',
+				})
+			);
+			const response = await fetch(
+				'https://react-http-37831-default-rtdb.firebaseio.com/cart.json',
+				{ method: 'PUT', body: JSON.stringify(cart) }
+			);
 
-	useEffect(() => {
+			if (!response.ok) {
+				throw new Error('Sending cart data failed');
+			}
+
+			const responseData = await response.json();
+
+			dispatch(
+				uiActions.showNotification({
+					status: 'success',
+					title: 'Success',
+					message: 'Sent cart data succesfully',
+				})
+			);
+		};
+
 		if (isInitial) {
 			isInitial = false;
 			return;
 		}
 
-		if (cart.changed) {
-			dispatch(sendCartData(cart));
-		}
+		sendCartData().catch((error) => {
+			dispatch(
+				uiActions.showNotification({
+					status: 'error',
+					title: 'Error',
+					message: 'Failed to send cart data',
+				})
+			);
+		});
 	}, [cart, dispatch]);
 
 	return (
@@ -48,5 +80,3 @@ function App() {
 }
 
 export default App;
-
-//const responseData = await response.json();
