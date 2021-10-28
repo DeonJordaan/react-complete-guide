63670cc17193774787ae4ba462c0b3bc3c024171
DeonJ76 <deon.jordaan76@gmail.com>
2021-10-28 21:03:52 +0200

    Section 24 - Animating React Apps - CSS Transitions & Animations

---
+++ 24-Animation/animations--01-start/src/App.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+import './App.css';
+import Modal from './components/Modal/Modal';
+import Backdrop from './components/Backdrop/Backdrop';
+import List from './components/List/List';
+
+class App extends Component {
+	state = {
+		modalIsOpen: false,
+	};
+
+	showModal = () => {
+		this.setState({ modalIsOpen: true });
+	};
+
+	closeModal = () => {
+		this.setState({ modalIsOpen: false });
+	};
+
+	render() {
+		return (
+			<div className="App">
+				<h1>React Animations</h1>
+				{this.state.modalIsOpen ? (
+					<Modal
+						show={this.state.modalIsOpen}
+						closed={this.closeModal}
+					/>
+				) : null}
+				{this.state.modalIsOpen ? (
+					<Backdrop show={this.state.modalIsOpen} />
+				) : null}
+				<button className="Button" onClick={this.showModal}>
+					Open Modal
+				</button>
+				<h3>Animating Lists</h3>
+				<List />
+			</div>
+		);
+	}
+}
+
+export default App;
