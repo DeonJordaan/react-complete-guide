6c025ff3ec8122509b5cd81569d139a53d5cf0ac
DeonJ76 <deon.jordaan76@gmail.com>
2021-11-01 18:40:57 +0200

    Section 25 complete - Replacing Redux with React Hooks

---
+++ 25-Replace-Redux/replace-redux-01-starting-project/src/containers/Products.js
@@ -0,0 +1,28 @@
+import React, { useContext } from 'react';
+
+import ProductItem from '../components/Products/ProductItem';
+// import { ProductsContext } from '../context/products-context';
+import { useStore } from '../hooks-store/store';
+import './Products.css';
+
+const Products = (props) => {
+	// const productList = useContext(ProductsContext).products;
+	// const [state, dispatch] = useStore(); don't need the dispatch so...
+	const state = useStore()[0]; // ...can access ONLY the state this way
+
+	return (
+		<ul className="products-list">
+			{state.products.map((prod) => (
+				<ProductItem
+					key={prod.id}
+					id={prod.id}
+					title={prod.title}
+					description={prod.description}
+					isFav={prod.isFavorite}
+				/>
+			))}
+		</ul>
+	);
+};
+
+export default Products;
